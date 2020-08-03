e21bf74c3e75c55873493d052ed86516b87d0d3f
Milan Mishra <hi.milan102@gmail.com>
2020-08-02 17:33:08 -0700

    Add finished home page

---
+++ nav.js
@@ -0,0 +1,25 @@
+document.write(`<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="d3.png" width="30" height="30" alt="" loading="lazy">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="slide1.html">Slide 1</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="slide2.html">Slide 2</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="slide3.html">Slide 3</a>
+      </li>
+    </ul>
+  </div>
+</nav>`
+);
