ff4c9f515e89e21c540fd7ca06ac8a1e69078488
Milan Mishra <hi.milan102@gmail.com>
2020-08-03 01:33:20 -0700

    Update image paths

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 document.write(`<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">
-    <img src="d3.png" width="30" height="30" alt="" loading="lazy">
+    <img src="images/d3.png" width="30" height="30" alt="" loading="lazy">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
